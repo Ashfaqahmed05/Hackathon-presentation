8f30ee90b351a4d5d388d83cef02cc77953d738d
Ashfaq Ahmed <135797310+Ashfaqahmed05@users.noreply.github.com>
2024-06-30 22:01:24 +0500

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import CertificateComponent from '../cirtificaate'
+
+const Home = () => {
+  return (
+    <Layout>
+        <CertificateComponent/>
+
+    </Layout>
+  )
+}
+
+export default Home
